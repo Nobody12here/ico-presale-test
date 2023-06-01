95570733ebadeb35cbe7d6fefbde74bd0605ff9a
Nobody12here <78294230+Nobody12here@users.noreply.github.com>
2023-06-01 18:12:25 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Saler from './components/Saler'
+
+function App() {
+  return (
+    <div className="App">
+      <Saler></Saler>
+    </div>
+  );
+}
+
+export default App;
